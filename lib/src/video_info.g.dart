529112a798bbdd916860dc66beb9f2c51b88f077
Alex <alex.tran1502@gmail.com>
2024-11-07 17:14:52 -0600

    chore: update namespace

--- lib/src/video_info.g.dart
+++ lib/src/video_info.g.dart
@@ -7,9 +7,9 @@ part of 'video_info.dart';
 // **************************************************************************
 
 VideoInfo _$VideoInfoFromJson(Map<String, dynamic> json) => VideoInfo(
-      height: json['height'] as int,
-      width: json['width'] as int,
-      duration: json['duration'] as int,
+      height: (json['height'] as num).toInt(),
+      width: (json['width'] as num).toInt(),
+      duration: (json['duration'] as num).toInt(),
     );
 
 Map<String, dynamic> _$VideoInfoToJson(VideoInfo instance) => <String, dynamic>{
