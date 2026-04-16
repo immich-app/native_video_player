fc79edb22f4b8a2c7360f583df8c97f2b773fb81
mertalev <101130780+mertalev@users.noreply.github.com>
2026-04-16 17:00:19 -0400

    Revert "feat: handle aspect ratio (#14)"
    This reverts commit 0cc59abad8216d4bb26d959252c6aca1a8d05ef9.

--- ios/Classes/NativeVideoPlayerView.swift
+++ ios/Classes/NativeVideoPlayerView.swift
@@ -17,7 +17,7 @@ class NativeVideoPlayerView: UIView {
 
         backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0)
 
-        playerLayer.videoGravity = .resizeAspect
+        playerLayer.videoGravity = .resize
         layer.addSublayer(playerLayer)
     }
 
